5a49184a4789137e64c625c251fd5bcf2abe0c0b
Luiz Otavio <luizotrv@gmail.com>
2017-06-10 18:24:40 -0300

    toys  and file_input_name_counter_string

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <malloc.h>
 #include <stdlib.h>
 
+#define FILE_BEFORE_SORT ".fbs"
+
 int main(int argc, char * argv[]){
 
 	FILE *input = NULL, *output = NULL;
@@ -14,32 +16,32 @@ int main(int argc, char * argv[]){
 	
 	char *dir_name = malloc(256);
 	strcpy(dir_name, argv[4]);
-	strcat(dir_name, "index");
+	strcat(dir_name, FILE_BEFORE_SORT);
 	output = fopen(dir_name, "w+");
 
 	line = malloc( (size_t) (memory_size+1) );
 	int files_counter;
 
-	char file_counter_string[5];
+	char file_input_name_counter_string[10]; // MAX number of inputs with 10 digits
 	for (files_counter = 1; files_counter <= (strtol(argv[1], &word, 10)); files_counter++) {
 
 		strcpy(dir_name, argv[3]);
-		sprintf(file_counter_string, "%d", files_counter);
-		strcat(dir_name, file_counter_string);
+		sprintf(file_input_name_counter_string, "%d", files_counter);
+		strcat(dir_name, file_input_name_counter_string);
 		input = fopen(dir_name, "r");
 
 		if(input != NULL) {
 			while( fread(line, 1, (size_t) memory_size, input) ) {
 				eof = (char) (strlen(line) == ((size_t)memory_size) ? '0' : '1');
 				word = strtok (line, " \n"); //  no memory is allocated, we're good at this point
-				while (word != NULL ) {
+				while (word != NULL && word != "" ) {
 					next_word = strtok (NULL, " \n");
 					if(next_word == NULL && eof == '0'){
 						// verify if the next word is null, then back it
 						fseek(input, -strlen(word)-1, SEEK_CUR);
 					} else {
 						fwrite(word, strlen(word), 1, output);
-						fwrite("\n", sizeof(char), 1, output);
+						fwrite("\n", 1, 1, output);
 					}
 					word = next_word;
 				}
